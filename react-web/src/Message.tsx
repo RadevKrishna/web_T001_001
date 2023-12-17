370d4814c5186c2cff3714743d2ff1738a8f11c9
RADEV KRISHNA MR <radevkrishna2003@gmail.com>
2023-12-17 21:05:30 +0530

    first react component

---
+++ react-web/src/Message.tsx
@@ -0,0 +1,13 @@
+
+//PascalCasing
+function Message(){
+
+    const name ='RADEV'
+    //JSX : JavaScript XML
+    if(name)
+        return <h1>Hello {name}</h1>;
+    return <h1>Hello World</h1>
+}
+
+
+export default Message;
